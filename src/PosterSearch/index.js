b840f30bd4269c2a66e18024bdc261d300c32b49
Brian <trangb@oregonstate.edu>
2019-11-18 23:54:39 -0800

    test case examples and sample code

--- src/PosterSearch/index.js
+++ src/PosterSearch/index.js
@@ -1,34 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function PosterSearch () {
+export default function PosterSearch() {
+  const [disableSearch, setDisableSearch] = useState(true);
+  const [movieName, setMovieName] = useState("");
+  const [msg, setMsg] = useState("");
+  const [posters,setPosters]=useState([])
+  function handleInput({ target: { value } }) {
+    setDisableSearch(value.length < 3);
+    setMovieName(value);
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    setMsg("Searching...");
+    fetch(
+      `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${encodeURIComponent(movieName)}`
+    ).then(resp=>resp.json())
+      .then(results=>{
+        if(results.Response==='True'){
+          setMsg(
+            `Now showing the first ${results.Search.length} results of ${results.totalResults}`
+          )
+          setPosters(results.Search)
+          setDisableSearch(false)
+        }
+        else{
+          if(results.Error==='Movie not found!'){
+            setMsg('Sorry, we couldn\'t find that one. Please try again.')
+          }
+          else{
+            setMsg(results.Error)
+          }
+        }
+      
+    })
+    .catch(err=>{
+      setMsg('Something went wrong. Please try again later.')
+    });
+  }
   return (
     <>
-      <section className='PosterSearch'>
-        <header className='header'>
+      <section className="PosterSearch">
+        <header className="header">
           <h1>Posterz</h1>
           <h3>Find your favourite movie posters.</h3>
         </header>
         <main>
           <p>
-            <label className='label' htmlFor='movie-name'>
+            <label className="label" htmlFor="movie-name">
               Movie title:
-            </label>{' '}
+            </label>{" "}
             <input
-              className='searchBox'
-              type='search'
-              id='movie-name'
-              name='movie-name'
-              placeholder='enter the name of a movie'
+              className="searchBox"
+              type="search"
+              id="movie-name"
+              name="movie-name"
+              placeholder="enter the name of a movie"
+              onChange={handleInput}
             />
-            <button id='search-button' className='searchButton'>
+            <button
+              id="search-button"
+              className="searchButton"
+              
+              disabled={disableSearch}
+              onClick={handleClick}
+            >
               Search
             </button>
             <br />
           </p>
-          <p id='msg' />
+          <p id="msg">{msg}</p>
         </main>
-        <section id='poster-grid' className='PosterGrid' />
+        <section id="poster-grid" className="PosterGrid">
+          {posters.map(poster=>{
+            return(<img
+              key={poster.Title}
+              src={poster.Poster}
+              alt={poster.Title}
+              title={poster.Title}
+            />)
+          })}
+      </section>
       </section>
     </>
-  )
+    );
 }
--- test/ui/PosterSearch.spec.js
+++ test/ui/PosterSearch.spec.js
@@ -6,22 +6,121 @@ import missingImages from '../fixtures/omdbapi/missing-images.json'
 import dotenv from 'dotenv'
 dotenv.config()
 describe('Poster Search', () => {
-  beforeAll(async () => {
-    browser.on('disconnected', () => {
+
+  beforeAll(async () => {   
+    browser.on('disconnected', () => { //when browser disconnects, close the browser
       browser.close()
     })
   })
-  beforeEach(async () => {
-    await jestPuppeteer.resetPage()
-    return page.goto('http://localhost:3000')
+  beforeEach(async () => { 
+    await jestPuppeteer.resetPage()   //restart page, gives us a 'virgin' page for each test
+    const url = process.env.NODE_ENV ==='build'? 'http://localhost:5000' : 'http://localhost:3000'
+    return page.goto(url)
   })
 
-  xit("doesn't let me search until I've typed at least 3 characters", () => {})
-  xit("tells me when we're searching", () => {})
+  it("doesn't let me search until I've typed at least 3 characters", async () => {
+    expect(await page.$eval('#search-button', btn => btn.disabled)).toBe(true)
+    await expect(page).toFill('#movie-name', 'abc')
+    return expect(await page.$eval('#search-button', btn => btn.disabled)).toBe(
+      false
+    )
+  })
+
+  it("tells me when we're searching", async (done) => {
+    expect.assertions(3);   //say there are 3 assertions in this test
+    await page.setRequestInterception(true)
+
+    page.on('request',async req=>{
+      if(req.url().includes('omdbapi.com')){
+        const msg = await page.$('#msg')
+        await expect(msg).toMatch('Searching...')
+
+        await req.respond({
+          headers:{"Access-Control-Allow-Origin":"*"},
+          body: JSON.stringify(dummyPosters),   //use stubbed out response for external apis
+          contentType:'application/json'
+        })
+        
+        done()
+
+      }
+    })
+    await expect(page).toFill('#movie-name','star')   //order puppeteer to  input 'star' into search called movie-name
+    await expect(page).toClick("#search-button")       //order puppeteer to click 'search-button'
+  })
   xit('tells me when there are no results', () => {})
-  xit('handles api errors', () => {})
-  xit('handles network errors', () => {})
+  it('handles api errors', async(done) => {
+    await page.setRequestInterception(true)
+    page.on('request',async req=>{
+      if(req.url().includes('omdbapi.com')){
+        await req.respond({
+          headers:{"Access-Control-Allow-Origin":"*"},
+          body:JSON.stringify(apiErrorResponse),
+          contentType:"application/json"
+        })
+      }
+    })
+    page.on('response',async(res)=>{
+      if(res.url().includes('omdbapi.com')){
+        const msg = await page.$('#msg')
+        await expect(msg).toMatch(apiErrorResponse.Error)
+        await page.setRequestInterception(false)
+        done()
+      }
+    })
+    await expect(page).toFill('#movie-name','star')
+    await expect(page).toClick('#search-button')
+  })
+  it('handles network errors', async(done) => {
+    expect.assertions(3)
+    await page.setRequestInterception(true)
+    page.on('request',async(req)=>{
+      if(req.url().includes('omdbapi.com')){
+        await req.abort('failed')
+        const msg = await page.$('#msg')
+        await expect(msg).toMatch('Something went wrong. Please try again later.')
+        await page.setRequestInterception(false)
+        done()
+      }
+    })
+    await expect(page).toFill('#movie-name','star')
+    await expect(page).toClick('#search-button')
+  })
   xit('tells me how many results were found and how many are being displayed', () => {})
-  xit('displays all results', () => {})
+  it('displays all results', async(done) => {   //main thing app is suppose to do
+    expect.assertions(12)
+    await page.setRequestInterception(true)
+    page.on('request',async req=>{
+      if(req.url().includes('omdbapi.com')){
+        if(process.env.NODE_ENV==='build'){
+          await req.continue()
+        }
+        else{
+          await req.respond({
+            headers:{"Access-Control-Allow-Origin":"*"},
+            body: JSON.stringify(dummyPosters),   //use stubbed out response for external apis
+            contentType:'application/json'
+          })
+        }
+      }
+    })
+
+    page.on('response',async res=>{
+      if(res.url().includes("omdbapi.com")){
+        const results = process.env.NODE_ENV ==='build'? await res.json(): dummyPosters
+        await Promise.all(
+          results.Search.map(movie=>{
+            return(expect(page).toMatchElement(`img[src="${movie.Poster}"]`))
+          }
+        )
+        ).then(async ()=>{
+          await page.setRequestInterception(false)
+          done()
+        })
+      }
+    })
+    await expect(page).toFill('#movie-name','star')
+    await expect(page).toClick('#search-button')
+  })
   xit('displays a placeholder when no poster is available', () => {})
 })
